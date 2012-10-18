b43e6a3b7648712374c3d7080e14dd420438bf1e
Josh Rolstad <jrolstad@gmail.com>
2012-10-18 06:30:50 -0700

    Updating tests

--- Simple.Logging.Aws.Tests/Integration/When_logging.cs
+++ Simple.Logging.Aws.Tests/Integration/When_logging.cs
@@ -15,8 +15,8 @@ namespace Simple.Logging.Aws.Tests.Integration
         public void Then_a_message_is_logged()
         {
             // Arrange
-            var awsAccessKeyId = "AKIAJKVLDDPWSGXZF2GQ";
-            var awsSecretAccessKey = "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";
+            var awsAccessKeyId = "<access key>";
+            var awsSecretAccessKey = "<secret key>";
 
             var simpelDbClient = new Amazon.SimpleDB.AmazonSimpleDBClient(awsAccessKeyId, awsSecretAccessKey);
             var s3Client = new Amazon.S3.AmazonS3Client(awsAccessKeyId, awsSecretAccessKey);
@@ -37,8 +37,8 @@ namespace Simple.Logging.Aws.Tests.Integration
         public void Then_an_Exception_is_logged()
         {
             // Arrange
-            var awsAccessKeyId = "AKIAJKVLDDPWSGXZF2GQ";
-            var awsSecretAccessKey = "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";
+            var awsAccessKeyId = "<access key>";
+            var awsSecretAccessKey = "<secret key>";
 
             var simpelDbClient = new Amazon.SimpleDB.AmazonSimpleDBClient(awsAccessKeyId, awsSecretAccessKey);
             var s3Client = new Amazon.S3.AmazonS3Client(awsAccessKeyId, awsSecretAccessKey);
